2dc10e9aea62b7f739bb515d5d6039f4c43c38cf
saakethch <123130490+saakethch@users.noreply.github.com>
2024-01-27 17:09:58 -0500

    Add source code

--- main.py
+++ main.py
@@ -18,38 +18,64 @@ app = FastAPI()
 connection_uri = "postgresql://<user>:<pass>@<host:port>/<db>"
 # pip install langchain openai uvicorn fastapi sqlalchemy pandas pydantic psycopg2
 class SQLTools():
+  """A collection of SQL tools for extracting JSON objects, retrieving table names, and executing SQL queries."""
+
   def extract_json(input_string):
+    """
+    Extracts the first JSON object found in the input string.
+
+    Args:
+      input_string (str): The input string to search for JSON objects.
+
+    Returns:
+      dict or None: The extracted JSON object as a dictionary, or None if no JSON object is found or if the found string is not a valid JSON.
+    """
     # Regular expression to find JSON objects
     json_regex = r'\{.*?\}'
     matches = re.findall(json_regex, input_string, re.DOTALL)
 
     if matches:
-        # Assuming you want to extract the first JSON object found
-        try:
-            return json.loads(matches[0])
-        except json.JSONDecodeError:
-            print("Found string is not a valid JSON.")
-            return None
-    else:
-        print("No JSON found in the input string.")
+      # Assuming you want to extract the first JSON object found
+      try:
+        return json.loads(matches[0])
+      except json.JSONDecodeError:
+        print("Found string is not a valid JSON.")
         return None
+    else:
+      print("No JSON found in the input string.")
+      return None
 
   @tool("Give db information result in json format")
   def get_table_list(sqlschema = "public"):
-    """Useful to get table names"""
+    """
+    Retrieves a list of table names from the specified SQL schema.
+
+    Args:
+      sqlschema (str, optional): The SQL schema to retrieve table names from. Defaults to "public".
+
+    Returns:
+      list: A list of table names.
+    """
     connection_uri = "postgresql://<user>:<pass>@<host:port>/<db>"
     engine = create_engine(connection_uri)
     sql = f"""
-        SELECT table_name from INFORMATION_SCHEMA.tables where table_schema = '{sqlschema}' ORDER BY table_name
+      SELECT table_name from INFORMATION_SCHEMA.tables where table_schema = '{sqlschema}' ORDER BY table_name
     """
     with engine.connect() as connection:
-        rows = list(connection.execute(text(sql)))
-        return [r[0] for r in  rows]
-#and give the result in json format OUTPUT Result PUERLY IN JSON FORMAT. Add your own judjement of 1-5 rating as a new feild farm_health in resulting json along with items from results
+      rows = list(connection.execute(text(sql)))
+      return [r[0] for r in  rows]
+
   @tool("Execute SQL query and give OUTPUT only the sql code in json format withe key being sql_code")
   def exec_sql(query):
-    """Useful to execute SQL queries and give the result in json format Execute SQL query only the sql code in json format withe key being sql_code """
-    # connection_uri = "postgresql://ukMqtt_user:bvWkDNvmVI7xQT38Ir@185.136.157.11:5432/ukMqtt"
+    """
+    Executes the specified SQL query and returns the result in JSON format.
+
+    Args:
+      query (str or dict): The SQL query to execute, either as a string or as a dictionary with the key 'sql_code' or 'query'.
+
+    Returns:
+      str or dict: The result of the SQL query in JSON format, or an error message if the query fails.
+    """
     try:
       query = json.loads(query) 
     except Exception as e:
@@ -60,18 +86,18 @@ class SQLTools():
     engine = create_engine(connection_uri)
 
     if query and query.lower().startswith("select"):
-        try:
-          last_results = pd.read_sql_query(f"""{query}""", engine)
-          print(last_results.to_json())
-          if last_results.shape[0] > 20:
-              return last_results.head(20).to_json()
-          else:
-              return last_results.to_json()
-        except Exception as e:
-          return {"error", f" Database query failed: {e}"}
+      try:
+        last_results = pd.read_sql_query(f"""{query}""", engine)
+        print(last_results.to_json())
+        if last_results.shape[0] > 20:
+          return last_results.head(20).to_json()
+        else:
+          return last_results.to_json()
+      except Exception as e:
+        return {"error", f" Database query failed: {e}"}
     else:
-        print("Invalid query: ", query)
-        return {"error": f"Failed to run non-select query Check your query with syntax , logic and relavancy to the NLQ '{query}' also only the sql code in json format withe key being sql_code"}
+      print("Invalid query: ", query)
+      return {"error": f"Failed to run non-select query Check your query with syntax , logic and relavancy to the NLQ '{query}' also only the sql code in json format withe key being sql_code"}
     
 
 class UrbiAgent():
@@ -89,7 +115,7 @@ class UrbiAgent():
   def agent_friend(self):
     return Agent(
         role='SQL Coder',
-        goal='Provide the BEST sql code to other agents remind them to while parsing the table names or field names wrap them around double qoutes which  is very important or user and then execute to get the results for the nlq by the user MAIN GOAL :: give me the result in json format using your tool',
+        goal='Provide the BEST sql code to other agents remind them to while parsing the table names or field names wrap them around double qoutes which is very important or user and then execute to get the results for the nlq by the user',
         backstory="""A knowledgeable sql developer with extensive information 
         about professional sql coding best practices and standards""",
         tools=[],
@@ -99,7 +125,7 @@ class UrbiAgent():
 class UrbiTasks():
   def gen_sql(self,agent, db_context,nlq):
     return Task(description=dedent(f"""
-        {db_context} -> given this context, generate a SQL query for the {nlq} natural language query AND THEN MAINLY USE YOUR TOOL TO EXECUTE THE QUERY USING THE TOOL AND GIVE THE RESULTS
+        {db_context} -> given this context, generate a SQL query for the {nlq} natural language query AND THEN MAINLY USE YOUR TOOL TO EXECUTE THE QUERY
       """),
                 agent=agent)
 
@@ -176,8 +202,8 @@ class QueryRequest(BaseModel):
 @app.post("/ask_farm")
 async def urbi_ask(request: QueryRequest):
   print('-------------------------------')
-  tablenames = "Farm_growareas_log_1"
-  nlq = request.nlq #"How many rows have a temperature greater than 30? MAIN GOAL :: give me the result in json format using your tool"
+  tablenames = "<table_name>"
+  nlq = request.nlq
   db_context = chat_db() +"For the table name: {tablenames} " 
   ghs = GrootHS(db_context,nlq)
   result = ghs.run()
